7173a6fc21c681284029a7dc30f517b2a25defeb
Note <vincent.van.lohuizen@realproduction.nl>
2026-04-15 17:32:18 +0200

    Optimize header and button layout

--- src/code/components/panels.ts
+++ src/code/components/panels.ts
@@ -4,10 +4,18 @@ const songListPanel = document.querySelector(
 const songViewPanel = document.querySelector(
   ".song-view-panel",
 ) as HTMLDivElement;
+const autoScroller = document.querySelector(".autoscroll") as HTMLDivElement;
+const btnBar = document.querySelector(".btn-bar") as HTMLDivElement;
+const fontSizeButtons = document.querySelector(
+  ".font-size-buttons",
+) as HTMLDivElement;
 
 export function togglePanels(): void {
   songListPanel.classList.toggle("hidden");
   songViewPanel.classList.toggle("hidden");
+  autoScroller.classList.toggle("hidden");
+  fontSizeButtons.classList.toggle("hidden");
+  btnBar.classList.toggle("hidden");
 }
 
 export function songViewIsHidden() {
